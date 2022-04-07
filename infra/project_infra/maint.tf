4afbc1dfd5947b9daab30a06e55a799338184202
Pablo Salva <mapfre-plataforma-chatbots-dev@emergya.com>
2022-04-07 18:59:07 +0200

    Force deploy

--- infra/project_infra/maint.tf
+++ infra/project_infra/maint.tf
@@ -18,3 +18,19 @@ module "cloudbuild_1" {
   regex_branchs = "main"
   cloudbuild_yml = "infra/cloudbuild_yaml/sample_one.yaml"
 }
+
+module "cloudbuild_gcloud_deploy" {
+  source = "./../modules/cloudbuild"
+
+  project_id = var.project_id
+  name = "gcloud-deploy-cloudbuild"
+  repository_owner = "PaSalva"
+  repository_name = "simpleappproject"
+  regex_branchs = "prod"
+  cloudbuild_yml = "infra/cloudbuild_yaml/gcloud_deploy_sample.yaml"
+  substitutions      = {
+    _PROJECT_ID = var.project_id
+  }
+}
+
+
