9779ef182fcceb3b1cd861ac327c10edc96a1b28
Pablo Salva <mapfre-plataforma-chatbots-dev@emergya.com>
2022-04-07 18:26:07 +0200

    Add app and cloudbuild

---
+++ infra/modules/cloudbuild/cloudbuild.tf
@@ -0,0 +1,48 @@
+###############
+## Variables ##
+###############
+variable "name" {}
+variable "repository_owner" {}
+variable "project_id" {}
+variable "repository_name" {}
+variable "regex_branchs" {}
+variable "cloudbuild_yml" {}
+
+###############
+##    APIS   ##
+###############
+
+variable "apis" {
+  default = [
+    "cloudbuild"
+  ]
+}
+
+resource "google_project_service" "apis" {
+  for_each = toset(var.apis)
+  project  = var.project_id
+  service  = "${each.key}.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+
+###############
+## Resources ##
+###############
+resource "google_cloudbuild_trigger" "trigger" {
+  name    = var.name
+  project = var.project_id  
+  github {
+    owner = var.repository_owner
+    name  = var.repository_name
+    push {
+      branch = var.regex_branchs
+    }
+  }
+  filename = var.cloudbuild_yml
+
+  depends_on = [
+    google_project_service.apis
+  ]
+}
--- infra/project_infra/maint.tf
+++ infra/project_infra/maint.tf
@@ -6,3 +6,15 @@ module "gke" {
   naming     = var.naming
 
 }
+
+
+module "cloudbuild_1" {
+  source = "./../modules/cloudbuild"
+
+  project_id = var.project_id
+  name = "sample-one-cloudbuild"
+  repository_owner = "PaSalva"
+  repository_name = "simpleappproject"
+  regex_branchs = "main"
+  cloudbuild_yml = "infra/cloudbuild_yaml/sample_one.yaml"
+}
